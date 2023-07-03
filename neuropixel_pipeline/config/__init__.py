1383e46d341448e6697ea4a6f4aa36e2c4f65726
Cpapa97 <cpapa97@gmail.com>
2023-07-03 15:28:24 -0500

    entertain the idea of saving pipeline_config to file

--- neuropixel_pipeline/config/__init__.py
+++ neuropixel_pipeline/config/__init__.py
@@ -2,7 +2,8 @@ from pydantic import BaseModel, Field
 from typing import Optional, Tuple
 from pathlib import Path
 
-
+# I can possibly use json serialization to save this to a file and check for this file automatically,
+# so a pipeline minion or docker image could have this config be made once
 class PipelineConfig(BaseModel):
     generic_directory_suffix: Optional[Tuple[Path, Path]] = Field(
         description="Allows a generic path to be used, with a common suffix replaced by the passed in path"
