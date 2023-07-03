3207e99b855cf234b38c8f03cb35a212e667c1dc
Cpapa97 <cpapa97@gmail.com>
2023-07-03 16:02:18 -0500

    wip note on how would a default config be chosen

--- neuropixel_pipeline/config/__init__.py
+++ neuropixel_pipeline/config/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 # I can possibly use json serialization to save this to a file and check for this file automatically,
 # so a pipeline minion or docker image could have this config be made once
 # TODO: Even more ideally, it could be stored and/or fetched from a PipelineConfig table at the root of the schema,
-#       that no other table should depend on directly.
+#       that no other table should depend on directly. TODO: How would a default be chosen though?
 class PipelineConfig(BaseModel):
     generic_directory_suffix: Optional[Tuple[Path, Path]] = Field(
         description="Allows a generic path to be used, with a common suffix replaced by the passed in path"
