c7e6edae15212ec0cc9eadfe83201bf21aabbd01
Cpapa97 <cpapa97@gmail.com>
2023-07-19 08:29:22 -0500

    set proper alias priority for ScanKey

--- neuropixel_pipeline/modules/atlab/common.py
+++ neuropixel_pipeline/modules/atlab/common.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field, conint
 
 class ScanKey(BaseModel):
     animal_id: conint(ge=0, le=2_147_483_647)
-    session_id: conint(ge=0, le=32_767) = Field(alias='session')
-    scan_id: conint(ge=0, le=32_767) = Field(alias='scan_idx')
+    session_id: conint(ge=0, le=32_767) = Field(alias='session', alias_priority=1)
+    scan_id: conint(ge=0, le=32_767) = Field(alias='scan_idx', alias_priority=1)
