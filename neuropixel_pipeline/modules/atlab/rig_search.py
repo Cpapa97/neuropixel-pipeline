33d471b07a3182ea3878a3b0e5ddccdc22a6f1bf
Cpapa97 <cpapa97@gmail.com>
2023-07-19 08:47:36 -0500

    fix rig search

--- neuropixel_pipeline/modules/atlab/rig_search.py
+++ neuropixel_pipeline/modules/atlab/rig_search.py
@@ -1,9 +1,11 @@
-def get_rig(scan_key: dict):
+from .common import ScanKey
+
+def get_rig(scan_key: ScanKey):
     import datajoint as dj
 
     experiment = dj.create_virtual_module("experiment", "pipeline_experiment")
 
-    session_rel = experiment.Session() & scan_key
+    session_rel = experiment.Session() & scan_key.model_dump(by_alias=True)
     experiment_rig = session_rel.fetch1("rig")
 
     return experiment_rig
