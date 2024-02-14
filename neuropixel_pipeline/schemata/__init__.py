053356a9c6b0a470666d4f03b0ef2b9e41544d62
Cpapa97 <cpapa97@gmail.com>
2024-02-14 15:05:12 -0600

    make pipeline_config available from schemata directly

--- neuropixel_pipeline/schemata/__init__.py
+++ neuropixel_pipeline/schemata/__init__.py
@@ -7,5 +7,6 @@ SCHEMA_PREFIX = "neuropixel_"
 from . import probe  # noqa: E402
 from . import ephys  # noqa: E402
 from . import config  # noqa: E402
+from .config import pipeline_config  # noqa: E402
 
 __all__ = ["probe", "ephys", "config"]
